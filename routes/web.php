030971c4056bea7e85cf12fe998dbdacace61550
Megan Noel <megan@cercatrova.agency>
2025-08-26 15:22:41 -0400

    load tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+Route::view('/', 'home');
+
+/* 
+    two ways to write this route.
+    this is the first, below is the second
 Route::get('/second', function () {
     return view('second');
 });
+*/
 
+Route::view('/second', 'second');
 
